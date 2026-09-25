c7162581f90fedb37b573b42df9fb7b45d4b11a1
JanKokobane <mashajan1991@gmail.com>
2026-09-25 16:14:27 +0200

    fix: allow cross-origin requests for booking API

--- backend/index.js
+++ backend/index.js
@@ -5,23 +5,7 @@ import bookingRoutes from './routes/bookingRoutes.js';
 
 const app = express();
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'http://127.0.0.1:5500',
-  process.env.FRONTEND_URL
-].filter(Boolean);
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
